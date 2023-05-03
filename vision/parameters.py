d2ed086c78add73460b2167c851021f3dd6647f3
Ewan Golfier <ewan.golfier@epfl.ch>
2023-05-03 14:43:51 +0200

    More calibration

--- vision/parameters.py
+++ vision/parameters.py
@@ -30,12 +30,12 @@ gaussian_kernel_size = 3
 gaussian_sigma = 2
 bilateral_d = 15
 bilateral_sigma_color = 75
-bilateral_sigma_depth = 75
+bilateral_sigma_dist = 75
 depth_threshold = 150
 min_blob_size = 8000
 
 # filters
 depth_frame_filters = []  # decimation, spatial, temporal, hole_filling
 color_frame_filters = []
-depth_image_filters = [filters.bilateral(15, 75, 75), filters.gaussian_blur(gaussian_kernel_size, gaussian_sigma), filters.threshold(depth_threshold, 'r')]  # gaussian_blur, threshold
+depth_image_filters = [filters.bilateral(bilateral_d, bilateral_sigma_color, bilateral_sigma_dist), filters.gaussian_blur(gaussian_kernel_size, gaussian_sigma), filters.threshold(depth_threshold, 'r')]  # gaussian_blur, threshold
 color_image_filters = []
